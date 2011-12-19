dda8053ae3bd580d361ea9885e4f45722f04554b
Yossi <yossi@ubuntu3>
2011-12-18 17:19:16 -0800

    moved draw() out to its own module

--- sets.py
+++ sets.py
@@ -1,68 +1,28 @@
-import os
-import cStringIO
 from random import shuffle, choice
-import Image, ImageOps
-from bmps.sprites import *
 
 class Card(object):
-    def __init__(self, **kw):
-        self.number = kw['number']
-        self.color = kw['color']
-        self.shade = kw['shade']
-        self.shape = kw['shape']
-        
-    def __repr__(self):
-        return '(%s %s %s %s)' % (self.number.center(len(max(Deck.numbers, key=len))),
-                                  self.color.center(len(max(Deck.colors, key=len))),
-                                  self.shade.center(len(max(Deck.shades, key=len))), 
-                                  self.shape.center(len(max(Deck.shapes, key=len))))
-    def draw(self):
-        shape_size = 50, 90
-        space = shape_size[0]//3
-        margin = 10
-        
-        if not os.path.isfile('shapes.png') \
-           or Image.open('shapes.png').size != (shape_size[0]*3, shape_size[1]*3):
-            #print "regenerating shapes.png... please stand by"
-            create('shapes.png', shape_size)
-        
-        number = Deck.numbers.index(self.number) + 1
-        x1 = Deck.shades.index(self.shade) * shape_size[0]
-        x2 = x1 + shape_size[0]
-        y1 = Deck.shapes.index(self.shape) * shape_size[1]
-        y2 = y1 + shape_size[1]
-
-        shapes = Image.open('shapes.png')
-        shape = shapes.crop( (x1,y1,x2,y2) )
-        
-        im = Image.new( 'L', (number*(shape_size[0]+space)-space, shape_size[1]), 'white' )
-        for n in xrange(number):
-            a = (shape_size[0]+space) * n
-            b = a + shape_size[0]
-            box = (a, 0, b, shape_size[1])
-            im.paste(shape, box)
-        im = ImageOps.colorize(im, self.color, 'white')
-
-        card = Image.new( 'RGB', ((shape_size[0]*3)+((margin+space)*2),
-                                 shape_size[1]   + (margin * 2)    ), 'white' )
-        card.paste(im, center(im, card.size))
-        #card.show()
-        self.card = cStringIO.StringIO()
-        card.save(self.card, 'PNG')
-
-class Deck(object):
     numbers = ('one', 'two', 'three')
     colors  = ('green', 'red', 'purple')
     shades  = ('filled', 'shaded', 'empty')
     shapes  = ('squiggle', 'diamond', 'oval')
     
+    def __init__(self, **attributes):
+        self.attributes = attributes
+        
+    def __repr__(self):
+        return '(%s %s %s %s)' % (attributes['number'].center(len(max(self.numbers, key=len))),
+                                  attributes['color'].center(len(max(self.colors, key=len))),
+                                  attributes['shade'].center(len(max(self.shades, key=len))), 
+                                  attributes['shape'].center(len(max(self.shapes, key=len))))
+
+class Deck(object):    
     def __init__(self):
         self.cards = []
 
-        for number in Deck.numbers:
-            for color in Deck.colors:
-                for shade in Deck.shades:
-                    for shape in Deck.shapes:
+        for number in Card.numbers:
+            for color in Card.colors:
+                for shade in Card.shades:
+                    for shape in Card.shapes:
                         self.cards.append( Card(number=number, color=color, shade=shade, shape=shape) )
 
         shuffle(self.cards)
