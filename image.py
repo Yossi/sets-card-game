c40d3e25c8e98d2f28c8f65fb5f506ff0b86a6fa
Yossi <yossi.rafelson@gmail.com>
2011-12-25 12:41:01 -0800

    something to do with image.py. i forget what

--- image.py
+++ image.py
@@ -9,31 +9,35 @@ def draw(**kw):
     space = shape_size[0]//3
     margin = 10
     
+    card = Image.new( 'RGB', ((shape_size[0]*3)+((margin+space)*2),
+                               shape_size[1]   + (margin * 2)    ), 'white' )    
+    
     if not os.path.isfile('shapes.png') \
        or Image.open('shapes.png').size != (shape_size[0]*3, shape_size[1]*3):
         #print "regenerating shapes.png... please stand by"
         create('shapes.png', shape_size)
     
-    number = Card.numbers.index(kw['number']) + 1
-    x1 = Card.shades.index(kw['shade']) * shape_size[0]
-    x2 = x1 + shape_size[0]
-    y1 = Card.shapes.index(kw['shape']) * shape_size[1]
-    y2 = y1 + shape_size[1]
+    try:
+        number = Card.numbers.index(kw['number']) + 1
+        x1 = Card.shades.index(kw['shade']) * shape_size[0]
+        x2 = x1 + shape_size[0]
+        y1 = Card.shapes.index(kw['shape']) * shape_size[1]
+        y2 = y1 + shape_size[1]
 
-    shapes = Image.open('shapes.png')
-    shape = shapes.crop( (x1,y1,x2,y2) )
-    
-    im = Image.new( 'L', (number*(shape_size[0]+space)-space, shape_size[1]), 'white' )
-    for n in xrange(number):
-        a = (shape_size[0]+space) * n
-        b = a + shape_size[0]
-        box = (a, 0, b, shape_size[1])
-        im.paste(shape, box)
-    im = ImageOps.colorize(im, kw['color'], 'white')
+        shapes = Image.open('shapes.png')
+        shape = shapes.crop( (x1,y1,x2,y2) )
+        
+        im = Image.new( 'L', (number*(shape_size[0]+space)-space, shape_size[1]), 'white' )
+        for n in xrange(number):
+            a = (shape_size[0]+space) * n
+            b = a + shape_size[0]
+            box = (a, 0, b, shape_size[1])
+            im.paste(shape, box)
+        im = ImageOps.colorize(im, kw['color'], 'white')
 
-    card = Image.new( 'RGB', ((shape_size[0]*3)+((margin+space)*2),
-                             shape_size[1]   + (margin * 2)    ), 'white' )
-    card.paste(im, center(im, card.size))
+        card.paste(im, center(im, card.size))
+    except Exception as e:
+        print e
     #card.show()
     ret = cStringIO.StringIO()
     card.save(ret, 'PNG')
