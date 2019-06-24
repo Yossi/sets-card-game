d78de94ecd892e7ec479db0b33e701dfceb2adec
Yossi Rafelson <yossi.rafelson@gmail.com>
2019-06-24 14:47:17 -0700

    get it working on py3

--- image.py
+++ image.py
@@ -1,21 +1,21 @@
 import os
 import traceback
-import cStringIO
-import Image, ImageOps, ImageDraw
+import io
+from PIL import Image, ImageOps, ImageDraw
 from sets import Card
 from bmps.sprites import create, center
 
 def draw(**kw):
-    shape_size = 50, 90
+    shape_size = (50, 90)
     space = shape_size[0]//3
     margin = 10
     
     card = Image.new( 'RGB', ((shape_size[0]*3)+((margin+space)*2),
-                               shape_size[1]   + (margin * 2)    ), 'white' )    
+                               shape_size[1]   + (margin * 2)    ), 'white' )
     
     if not os.path.isfile('shapes.png') \
        or Image.open('shapes.png').size != (shape_size[0]*3, shape_size[1]*3):
-        #print "regenerating shapes.png... please stand by"
+        print("regenerating shapes.png... please stand by")
         create('shapes.png', shape_size)
     
     try:
@@ -29,7 +29,7 @@ def draw(**kw):
         shape = shapes.crop( (x1,y1,x2,y2) )
         
         im = Image.new( 'L', (number*(shape_size[0]+space)-space, shape_size[1]), 'white' )
-        for n in xrange(number):
+        for n in range(number):
             a = (shape_size[0]+space) * n
             b = a + shape_size[0]
             box = (a, 0, b, shape_size[1])
@@ -37,19 +37,19 @@ def draw(**kw):
         im = ImageOps.colorize(im, kw['color'], 'white')
 
         card.paste(im, center(im, card.size))
-    except Exception as e:
+    except Exception:
         draw = ImageDraw.Draw(card)
         error = traceback.format_exc()
         attributes = ' '.join(kw.values())
         for n, errLine in enumerate([attributes] + error.split('\n')):
             draw.text((0,15*n), errLine, fill='black')
-    #card.show()
+    card.show()
     
-    ret = cStringIO.StringIO()
+    ret = io.BytesIO()
     card.save(ret, 'PNG')
     return ret
     
 if __name__ == '__main__':
     # testing code follows. not expected to be left in working order
-    c = draw(number='one', color='lue', shade='filled', shape='oval')
+    c = draw(number='three', color='red', shade='solid', shape='oval')
 
